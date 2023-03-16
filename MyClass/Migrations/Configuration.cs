5972966c68e52046636b75bf5317d196f94372f4
LuongXuanNhat <xuannhat1932002@gmail.com>
2023-03-16 10:37:29 +0700

    tạo migrations

---
+++ MyClass/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace MyClass.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MyClass.Models.MyDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MyClass.Models.MyDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
