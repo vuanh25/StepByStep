4bfe582ad9fc93512dbe58667b50b8847810c96e
LuongXuanNhat <xuannhat1932002@gmail.com>
2023-03-16 09:53:11 +0700

    Them class Luyện Tập

---
+++ MyClass/Models/LuyenTap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClass.Models
+{
+    class LuyenTap
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required]
+        [StringLength(2000)]
+        public string NoiDungLuyenTap { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string TenLuyenTap { get; set; }
+
+        public virtual NgonNgu NgonNgu { get; set; }
+
+
+    }
+}
