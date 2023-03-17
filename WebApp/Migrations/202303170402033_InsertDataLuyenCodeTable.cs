eb54edf518a3fe8c54576177183db9eb013d017e
LuongXuanNhat <xuannhat1932002@gmail.com>
2023-03-17 11:38:22 +0700

    Thêm dữ liệu bảng Luyện Code và Chi tiết bài luyện

---
+++ WebApp/Migrations/202303170402033_InsertDataLuyenCodeTable.cs
@@ -0,0 +1,17 @@
+namespace WebApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InsertDataLuyenCodeTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO LuyenCodes (TenLuyenTap,YeuThich,DoKho,NgonNgu) VALUES (N'Hello World',1000,0,0)");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
