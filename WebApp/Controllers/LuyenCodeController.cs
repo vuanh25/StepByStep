17313ed73d5ac0426da5015c1e779167d419c752
LuongXuanNhat <xuannhat1932002@gmail.com>
2023-03-18 11:45:22 +0700

    oke

---
+++ WebApp/Controllers/LuyenCodeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class LuyenCodeController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: LuyenCode
+        public ActionResult Index()
+        {
+            var all_baitap = from tt in db.LuyenTaps select tt;
+            return View(all_baitap);
+        }
+
+    }
+}
