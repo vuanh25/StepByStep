8e49fbd9b0ad8e217caf13ede9d01a10948d5a25
LuongXuanNhat <xuannhat1932002@gmail.com>
2023-03-20 13:38:35 +0700

    Thêm các chức năng cho luyện code Admin

---
+++ WebApp/Areas/Admin/Controllers/LuyenCodeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+using WebApp.Models.Entities;
+
+namespace WebApp.Areas.Admin.Controllers
+{
+    public class LuyenCodeController : Controller
+    {
+        // GET: Admin/LuyenCode
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+
+        public ActionResult Index()
+        {
+            List<LuyenCode> BaiLuyen = db.LuyenTaps.ToList();
+            return View(BaiLuyen);
+
+          
+        }
+    }
+}
